905b8074b164ca24767dfb832868474a5e02311c
Imran <Imrvn@outlook.com>
2023-08-26 22:36:51 +0530

    revised

--- core/core/urls.py
+++ core/core/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings # new
-from  django.conf.urls.static import static #new
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,6 +29,3 @@ urlpatterns = [
     path('premod/', include('premod.urls')),     # rout for premod
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
